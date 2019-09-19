5f434a5a51e4256a6208ae91dc545bccddf7f997
le-moe <maurin.verrijdt@gmail.com>
2019-09-19 14:10:01 +0200

    [FIX] height of maze ascii repr

--- maze.py
+++ maze.py
@@ -1,11 +1,13 @@
+import math
 import random
+from cell import *
 
 """ Coordinate
 
       0 1 2 3 4 5 6 X
-0    / \_/ \_/ \_/ \
-1    \_/ \_/ \_/ \_/
-2    / \_/ \_/ \_/ \
+0    / \_/ \_/ \_/ \_
+1    \_/ \_/ \_/ \_/ \
+2    / \_/ \_/ \_/ \_/
 3    \_/ \_/ \_/ \_/
 Y
 
@@ -14,78 +16,16 @@ Y
 maze repr goal:
 
  _   _   _   _
-/ \_/ \_/  _/ \
-\_    / \_/  _/
-/ \_/  _  \_  \
-\_  \_/ \    _/
-/ \_  \_  \_/ \
-\_   _  \_   _/
-/ \_/ \_/ \_  \
+/ \_/ \_/  _/ \_
+\_    / \_/  _  \
+/ \_/  _  \_  \_/
+\_   _/ \    _/ \
+/ \_  \_  \_/  _/
+\_   _  \_   _  \
+  \_/ \_/ \_/ \_/
 
 
 """
-
-class Cell():
-    """ Hexagonal cell """
-    def __init__(self, x, y, walls=[0,1,2,3,4,5]):
-        """ x,y int
-            walls : list of number 0..5
-        """
-        assert((x+y)%2==0), "Invalide cell coordinate %s, %s" % (x,y)
-        self.x = x
-        self.y = y
-        self.walls = set(walls)
-        self.in_maze = False
-
-    def __repr__(self):
-        return '<%s, %s, %s>' % (self.x, self.y, self.walls)
-
-    def __eq__(self, other):
-        return (self.x == other.x) and (self.y == other.y) and (self.walls == other.walls)
-    def dir_to(self, other):
-        """ return direction from current cell to other cell
-            value: 0..5 """
-        if self.is_adjacent(other):
-            if self.x == other.x:
-                return 0 if (self.y > other.y) else 3
-            if self.x < other.x:
-                return 1 if self.y > other.y else 2
-            if self.x > other.x:
-                return 5 if self.y > other.y else 4
-
-    def is_adjacent(self, other):
-        """ return true if the two cells are adjacent.
-            That is, if they can share a wall in comon
-        """
-        if self.y == other.y:
-            return False
-        if self.y == other.y + 1:
-            return (self.x == other.x + 1) or (self.x == other.x - 1)
-        if self.y == other.y - 1:
-            return (self.x == other.x + 1) or (self.x == other.x - 1)
-        if self.x == other.x:
-            return (self.y == other.y + 2) or (self.y == other.y - 2)
-
-    def connect(self, other):
-        if self.is_adjacent(other):
-            direction = self.dir_to(other)
-            try:
-                self.walls.remove(direction)
-                other.walls.remove((direction+3) % 6)
-            except:
-                pass
-
-    def __contains__(self, wall):
-        # 4 in cell
-        return wall in self.walls
-
-    def labeled_walls(self):
-        labeled_walls = []
-        id = '%s:%s' % (self.x, self.y)
-        for w in self.walls:
-            label = '%s_%s' % (id,w)
-            labeled_walls.append(label)
-        return labeled_walls
 
 class Maze():
     """ Maze of hexagonal cell"""
@@ -103,6 +43,8 @@ class Maze():
     def __repr__(self):
         repr = "<maze %s, %s>\n" % (self.width, self.height)
         repr += self._to_ascii()
+        # for c in self.cells:
+        #     repr += '%s\n' % c
         return repr
 
     def _to_ascii(self):
@@ -111,8 +53,8 @@ class Maze():
             Y : vertical axis
         """
         # initialize the window
-        win_w = 5*(self.width-2)
-        win_h = self.height+3
+        win_w = 4*self.width+1
+        win_h = 2*(math.ceil(self.height/2))+1+(1-self.height%2)
         win = [[' ' for _ in range(win_h)] for _ in range(win_w)]
         for c in self.cells:
             x = 2*c.x + 1
@@ -129,6 +71,8 @@ class Maze():
                 win[x-1][y+1] = '\\'
             if 5 in c:
                 win[x-1][y] = '/'
+            if c.inside:
+                win[x][y] = c.inside
         s = '--------------\n'
         for y,_ in enumerate(win[0]):
             for x,_ in enumerate(win):
@@ -150,10 +94,19 @@ class Maze():
     #             label = '%s_%s' % (id,w)
     #             list_walls.append(label)
     #     return list_walls
-    def cell_from_coord(self, x,y):
+    def cell_at_coord(self, x,y):
         for c in self.cells:
             if (c.x, c.y) == (x,y):
                 return c
+
+    def connect(self, c1, c2):
+        if c1.is_adjacent(c2):
+            direction = c1.dir_to(c2)
+            try:
+                c1.walls.remove(direction)
+                c2.walls.remove((direction+3) % 6)
+            except:
+                pass
 
     def neighbour_cell(self, cell, direction):
         """ return the neighbour cell from cell given in arguments by following direction """
@@ -175,8 +128,14 @@ class Maze():
         elif direction==5:
             x = cell.x -1
             y = cell.y -1
-        for c in self.cells:
-            return cell_from_coord(x,y)
+        return self.cell_at_coord(x,y)
+
+    def all_neighbour(self, cell):
+        all_neighbour = []
+        for d in range(6):
+            if self.neighbour_cell(cell, d):
+                all_neighbour.append(self.neighbour_cell(cell, d))
+        return all_neighbour
 
     def randomized_kruskal(self):
         """ 1. Create a list of all walls, and create a set for each cell, each containing just that one cell.
@@ -252,5 +211,10 @@ def test_cell():
 
 if __name__ == "__main__":
     test_cell()
-    maze = Maze(20,20)
+    maze = Maze(10,10)
+    density = 500
+    for i in range(density):
+        c = random.choice(maze.cells)
+        neighbours = maze.all_neighbour(c)
+        maze.connect(c, random.choice(neighbours))
     print(maze)
